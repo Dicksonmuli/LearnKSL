b59424a1f4cd2cfa4ebc538dd38dfb5b88d9ec70
Gathua Kiragu <kiragugathua@gmail.com>
2017-07-07 09:35:14 +0300

    Redesigned the Detail Fragment

--- app/src/main/java/com/learnksl/learnksl/ui/DetailFragment.java
+++ app/src/main/java/com/learnksl/learnksl/ui/DetailFragment.java
@@ -1,11 +1,15 @@
 package com.learnksl.learnksl.ui;
 
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 import com.learnksl.learnksl.R;
 
@@ -26,5 +30,6 @@ public class DetailFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false);
     }
+
 
 }
