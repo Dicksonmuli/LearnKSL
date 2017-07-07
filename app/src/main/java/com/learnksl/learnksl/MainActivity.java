a50b93dac6c643e37af85bbf9ed89915e7966b55
dicksonmuli <dicksonmully6@gmail.com>
2017-07-07 13:37:13 +0300

    set button1 to onClickListener in MainActivity

--- app/src/main/java/com/learnksl/learnksl/MainActivity.java
+++ app/src/main/java/com/learnksl/learnksl/MainActivity.java
@@ -2,13 +2,25 @@ package com.learnksl.learnksl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //binding views
+    @Bind(R.id.button1) Button mButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mButton1.setOnClickListener(this);
     }
+    //override onclick method
+
 
 }
