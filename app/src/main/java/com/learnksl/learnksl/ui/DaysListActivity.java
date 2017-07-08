797ffe7d46e682ffbe81f74104ebdc357357b87f
dicksonmuli <dicksonmully6@gmail.com>
2017-07-09 02:15:14 +0300

    add activities for alphabet, days, months and numbers

---
+++ app/src/main/java/com/learnksl/learnksl/ui/DaysListActivity.java
@@ -0,0 +1,12 @@
+package com.learnksl.learnksl.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DaysListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
