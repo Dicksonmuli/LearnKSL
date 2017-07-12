7f7ec161fa903b200856b686e4699831e636e9c2
dicksonmuli <dicksonmully6@gmail.com>
2017-07-12 17:47:35 +0300

    DetailFragment - wrapping months and unwrapping on onCreate method

--- app/src/main/java/com/learnksl/learnksl/ui/DetailFragment.java
+++ app/src/main/java/com/learnksl/learnksl/ui/DetailFragment.java
@@ -10,25 +10,59 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.learnksl.learnksl.R;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DetailFragment extends Fragment {
+    @Bind(R.id.restaurantImageView) ImageView mImageLabel;
+    @Bind(R.id.restaurantNameTextView) TextView mNameLabel;
+
+    private ArrayList<String> mDbMonths;
+    private int mPosition;
+    private String mMonth;
 
 
-    public DetailFragment() {
-        // Required empty public constructor
+    public static DetailFragment newInstace(ArrayList<String> months, Integer position){
+        // wrapping months with parcels for serialization
+        DetailFragment detailFragment = new DetailFragment();
+        Bundle args = new Bundle();
+        args.putParcelable("months", Parcels.wrap(months));
+        args.putInt("position", position);
+        detailFragment.setArguments(args);
+        return detailFragment;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        unwrapping restaurant on onCreate
+        mDbMonths = Parcels.unwrap(getArguments().getParcelable("months"));
+        mPosition = getArguments().getInt("positon");
+        mMonth = mDbMonths.get(mPosition);
+//        invoke the menu items
+//        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_months_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        ButterKnife.bind(this, view);
+
+
     }
 
 
