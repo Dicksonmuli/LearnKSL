df0b0414aa375cd5be0b95f47a079fe1ef1ccb42
Gathua Kiragu <kiragugathua@gmail.com>
2017-07-13 12:17:24 +0300

    Added an onClickListener on fingerSpellingTextView Links the Communication Fingerspelling view to the FingerSpelling Activity

--- app/src/main/java/com/learnksl/learnksl/ui/CommunicationActivity.java
+++ app/src/main/java/com/learnksl/learnksl/ui/CommunicationActivity.java
@@ -1,15 +1,38 @@
 package com.learnksl.learnksl.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.learnksl.learnksl.MainActivity;
 import com.learnksl.learnksl.R;
 
-public class CommunicationActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CommunicationActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.fingersTextView)
+    TextView mFingersTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.X);
+        setContentView(R.layout.activity_communication);
+        ButterKnife.bind(this);
+
+        mFingersTextView.setOnClickListener(this);
+    }
+    //override onclick method
+
+    @Override
+    public void onClick(View view) {
+        if (view == mFingersTextView) {
+            Intent intent = new Intent(CommunicationActivity.this, FingerSpellingActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
