ca039f40bb57fec244459982deabcda9ee54601a
dicksonmuli <dicksonmully6@gmail.com>
2017-07-14 12:19:08 +0300

    changes in communication layout

--- app/src/main/java/com/learnksl/learnksl/MainActivity.java
+++ app/src/main/java/com/learnksl/learnksl/MainActivity.java
@@ -9,8 +9,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.learnksl.learnksl.ui.BasicsListActivity;
+import com.learnksl.learnksl.ui.CommunicationActivity;
+import com.learnksl.learnksl.ui.FeedbackActivity;
+import com.learnksl.learnksl.ui.TutorialsActivity;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -20,8 +24,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     //binding views
-    @Bind(R.id.restaurantImageView)
+    @Bind(R.id.basicsImageView)
     ImageView mButton1;
+    @Bind(R.id.basicsTextView)
+    TextView mBasicsTextView;
+    @Bind(R.id.tutorialsImageView)
+    ImageView mTutorialImageView;
+    @Bind(R.id.tutorialsTextView)
+    TextView mTutorialsTextView;
+    @Bind(R.id.communicationImageView)
+    ImageView mCommunicationImageView;
+    @Bind(R.id.communicationTextView)
+    TextView mCommunicationTextView;
+    @Bind(R.id.ImageView3)
+    ImageView mFeedbackImageView;
+    @Bind(R.id.feedbackTextView)
+    TextView mFeedbackTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +48,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         mButton1.setOnClickListener(this);
+
+        mButton1.setOnClickListener(this);
+        mBasicsTextView.setOnClickListener(this);
+        mCommunicationImageView.setOnClickListener(this);
+        mCommunicationTextView.setOnClickListener(this);
+        mTutorialImageView.setOnClickListener(this);
+        mTutorialsTextView.setOnClickListener(this);
+        mFeedbackImageView.setOnClickListener(this);
+        mFeedbackTextView.setOnClickListener(this);
+
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open,R.string.close);
 
@@ -38,11 +67,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
+
     //override onclick method
     @Override
     public void onClick(View view) {
         if (view == mButton1) {
             Intent intent = new Intent(MainActivity.this, BasicsListActivity.class);
+            startActivity(intent);
+        }
+        if (view == mBasicsTextView) {
+            Intent intent = new Intent(MainActivity.this, BasicsListActivity.class);
+            startActivity(intent);
+        }
+        if (view == mTutorialImageView) {
+            Intent intent = new Intent(MainActivity.this, TutorialsActivity.class);
+            startActivity(intent);
+        }
+        if (view == mTutorialsTextView) {
+            Intent intent = new Intent(MainActivity.this, TutorialsActivity.class);
+            startActivity(intent);
+        }
+        if (view == mCommunicationImageView) {
+            Intent intent = new Intent(MainActivity.this, CommunicationActivity.class);
+            startActivity(intent);
+        }
+        if (view == mCommunicationTextView) {
+            Intent intent = new Intent(MainActivity.this, CommunicationActivity.class);
+            startActivity(intent);
+        }
+        if (view == mFeedbackImageView) {
+            Intent intent = new Intent(MainActivity.this, FeedbackActivity.class);
+            startActivity(intent);
+        }
+        if (view == mFeedbackTextView) {
+            Intent intent = new Intent(MainActivity.this, FeedbackActivity.class);
             startActivity(intent);
         }
     }
