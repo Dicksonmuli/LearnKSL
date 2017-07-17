ca0d81196fc77dc7c33cc15944629ceab27dc7e9
wmbotela <wmbotela@gmail.com>
2017-07-17 16:36:59 +0300

    Updated Tutorial activity with youtubeviewer now you tube videos playing

--- app/src/main/java/com/learnksl/learnksl/ui/TutorialsActivity.java
+++ app/src/main/java/com/learnksl/learnksl/ui/TutorialsActivity.java
@@ -1,15 +1,63 @@
 package com.learnksl.learnksl.ui;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 import com.learnksl.learnksl.R;
 
-public class TutorialsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    @Override
+public class TutorialsActivity extends YouTubeBaseActivity {
+    YouTubePlayerView mYoutubePlayerView;
+    Button btnPlay;
+    YouTubePlayer.OnInitializedListener mOnInitializedListener;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorials);
+        btnPlay=(Button) findViewById(R.id.btnPlay);
+        mYoutubePlayerView = (YouTubePlayerView) findViewById(R.id.youtubePlay);
+
+        mOnInitializedListener = new YouTubePlayer.OnInitializedListener(){
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b){
+               // List<String> videoList = new ArrayList<>();
+               // videoList.add("");
+                //videoList.add("");
+                //youTubePlayer.loadVideos(videoList);
+                youTubePlayer.loadPlaylist("PLCe_vigJzMrP6yac5tNAfQNeQpg2fSPWR");
+            }
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult){
+
+            }
+        };
+        btnPlay.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mYoutubePlayerView.initialize(YouTubeConfig.getApiKey(), mOnInitializedListener);
+            }
+        });
+
+
+//        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+//        MediaController mediaController = new MediaController(this);
+//        mediaController.setAnchorView(videoView);
+//        Uri uri = Uri.parse("https://www.youtube.com/watch?v=z2S2XsZLDlI");
+//        videoView.setMediaController(mediaController);
+//        videoView.setVideoURI(uri);
+//        videoView.requestFocus();
+//        videoView.start();
+
     }
 }
---
+++ app/src/main/java/com/learnksl/learnksl/ui/YouTubeConfig.java
@@ -0,0 +1,16 @@
+package com.learnksl.learnksl.ui;
+
+/**
+ * Created by william on 7/17/17.
+ */
+
+public class YouTubeConfig {
+    public  YouTubeConfig(){
+
+    }
+    private static final String API_KEY ="AIzaSyB2Ig0Ro_5I86msNzupHBj42QZ_PatImhg";
+
+    public static String getApiKey(){
+        return API_KEY;
+    }
+}
