aab1912e5816c76bccc24f44bf3f93de98dc26ff
Gathua Kiragu <kiragugathua@gmail.com>
2017-07-18 09:19:39 +0300

    updates

--- app/src/main/java/com/learnksl/learnksl/ui/AlphabetsListActivity.java
+++ app/src/main/java/com/learnksl/learnksl/ui/AlphabetsListActivity.java
@@ -12,8 +12,5 @@ public class AlphabetsListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alphabets_detail);
 
-//        Intent intent = new Intent(this, DetailActivity.class);
-//        startActivity(intent);
-
     }
 }
