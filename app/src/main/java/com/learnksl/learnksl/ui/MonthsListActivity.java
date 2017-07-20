0111e2bb9a44552adece53a22e31d971d713c253
dicksonmuli <dicksonmully6@gmail.com>
2017-07-20 09:57:07 +0300

    MonthsListActivity - Youtube layout for a time, months and days vidio

--- app/src/main/java/com/learnksl/learnksl/ui/MonthsListActivity.java
+++ app/src/main/java/com/learnksl/learnksl/ui/MonthsListActivity.java
@@ -7,11 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,11 +32,38 @@ import java.util.List;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MonthsListActivity extends AppCompatActivity {
+public class MonthsListActivity extends YouTubeBaseActivity {
+    YouTubePlayerView mYoutubePlayerView;
+    Button btnPlay;
+    YouTubePlayer.OnInitializedListener mOnInitializedListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_months);
+
+        btnPlay=(Button) findViewById(R.id.monthsBtnPlay);
+        mYoutubePlayerView = (YouTubePlayerView) findViewById(R.id.monthsYoutubePlay);
+
+        mOnInitializedListener = new YouTubePlayer.OnInitializedListener(){
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b){
+                List<String> videoList = new ArrayList<>();
+//                videoList.add("ZkJsvB3Us_Y");
+//                videoList.add("dLx-fHpLXyY");
+                youTubePlayer.loadVideos(videoList);
+//                youTubePlayer.loadPlaylist("VyI1wtplk3Y");
+            }
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult){
+
+            }
+        };
+        btnPlay.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mYoutubePlayerView.initialize(YouTubeConfig.getApiKey(), mOnInitializedListener);
+            }
+        });
 
     }
 
