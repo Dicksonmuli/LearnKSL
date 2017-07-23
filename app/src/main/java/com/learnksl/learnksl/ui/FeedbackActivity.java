26d8d2d5890dc1e2f402d7fdccdefaa6c3b1698a
wmbotela <wmbotela@gmail.com>
2017-07-23 14:40:52 +0300

    feedback activity update

--- app/src/main/java/com/learnksl/learnksl/ui/FeedbackActivity.java
+++ app/src/main/java/com/learnksl/learnksl/ui/FeedbackActivity.java
@@ -4,18 +4,36 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.learnksl.learnksl.R;
 
 public class FeedbackActivity extends AppCompatActivity {
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mToggle;
+
+    EditText editName;
+    Button buttonSend;
+//    private DrawerLayout mDrawerLayout;
+//    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        editName =(EditText) findViewById(R.id.editName);
+        buttonSend = (Button) findViewById(R.id.buttonSend);
+
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    addFeedback();
+            }
+        });
 
 
 //   Navigation menu(Hamburger menu)
@@ -34,5 +52,15 @@ public class FeedbackActivity extends AppCompatActivity {
 //            return true;
 //        }
 //        return super.onOptionsItemSelected(item);
+   }
+
+   private void addFeedback(){
+       String name = editName.getText().toString().trim();
+
+       if(!TextUtils.isEmpty(name)){
+
+       }else{
+           Toast.makeText(this,"Please Enter your name", Toast.LENGTH_LONG).show();
+       }
    }
 }
